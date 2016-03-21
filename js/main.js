ba1a8fe0d33834b5c9ae03464a990d7ccc1d51b1
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-03-21 16:41:34 -0400

    fixed html

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ var $picker = $('form');
 var $input = $('#colour-picker');
 
 $picker.on('change', function () {
-	console.log('works');
-	$ball.css('background-color', $input.val());
+  console.log('works');
+  $ball.css('background-color', $input.val());
 });
